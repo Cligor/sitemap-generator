b162c26b5636842e8949c0e3c1f5e7f70d87bb76
cligor <willian.cligor@upcode.net.br>
2020-05-19 16:51:56 -0300

    Generating text sitemap

--- __main__.py
+++ __main__.py
@@ -1,15 +1,17 @@
 import psycopg2
-from config import databaseConfig
+from config import getConfig
 
-def connect():
+def __exec__():
     """ Connect to the PostgreSQL database server """
     conn = None
 
     try:
-        params = databaseConfig()
+        databaseParams = getConfig('database')
+        fileParms = getConfig('createdFile')
+        urlParms = getConfig('url')
 
         print('Connecting to the database')
-        conn = psycopg2.connect(**params)
+        conn = psycopg2.connect(**databaseParams)
 
         cur = conn.cursor()
 
@@ -18,6 +20,15 @@ def connect():
 
         db_version = cur.fetchone()
         print(db_version)
+
+        result = None
+        cur.execute('query')
+        result = cur.fetchall()
+
+        sitemap = open('{0}/{1}' .format(fileParms['path'], fileParms['name']), 'w+')
+
+        for data in result:
+            sitemap.write('{0}?{1}={2}\r' .format(urlParms['url'], urlParms['param'], data[0]))
 
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -28,4 +39,4 @@ def connect():
             print('Database connection closed')
 
 if __name__ == '__main__':
-    connect()
+    __exec__()
